7c16f44317877a3ebe8014a9f5a181fed2abe22e
Dorin <d.dumitrascuta@gmail.com>
2020-10-21 22:03:13 +0100

    import css to apply styles for image and add og tags

--- src/components/news-article.js
+++ src/components/news-article.js
@@ -2,17 +2,23 @@ import React from 'react'
 import SbEditable from 'storyblok-react'
 import Components from './components.js';
 import { StoryblockFormatter } from '../utils/RichTextFormatter';
-
+import "./layout.css";
+import { Helmet } from "react-helmet";
 
 
 const Feature = ({ blok, children }) => {
 	
 	return (
 		<SbEditable content={blok}>
+		<Helmet>
+			<meta property="og:image" content={blok.cover_image?.filename || ""} />
+			<meta property="og:image:width" content="400" />
+			<meta property="og:image:width" content="50" />
+		</Helmet>
 		<div className="col-8">
 			<h1>{blok.title}</h1>
 			{/* 
-			
+
 				I am an inline image e.g. cover 
 				<img src={blok.cover_image.filename} /> 
 				
