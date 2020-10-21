5a6e00325546eb6d060a87c7e83ef3aa964976c4
Dorin <d.dumitrascuta@gmail.com>
2020-10-21 21:38:24 +0100

    also embed an image, working as well..

--- src/components/news-article.js
+++ src/components/news-article.js
@@ -5,14 +5,21 @@ import { StoryblockFormatter } from '../utils/RichTextFormatter';
 
 
 
-const Feature = (props) => {
+const Feature = ({ blok, children }) => {
 	
 	return (
-		<SbEditable content={props.blok}>
+		<SbEditable content={blok}>
 		<div className="col-8">
-			<h1>{props.blok.title}</h1>
-			<div>{props.blok.caption}</div>
-			<content dangerouslySetInnerHTML={{__html: StoryblockFormatter(props.blok.content)}}></content>
+			<h1>{blok.title}</h1>
+			{/* 
+			
+				I am an inline image e.g. cover 
+				<img src={blok.cover_image.filename} /> 
+				
+			*/}
+			<div>{blok.caption}</div>
+			<content dangerouslySetInnerHTML={{__html: StoryblockFormatter(blok.content)}}></content>
+			<div>Tagged: {blok.tags}</div>
 		</div>
 		</SbEditable>
 	)
